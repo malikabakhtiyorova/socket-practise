c15c4323da545f50d82e9061bc38b56eaf5c7dd9
Malika Bakhtiyorova <malikabakhtiyorova@yandex.ru>
2021-02-06 17:40:37 +0500

    Initialize server

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const app = express();
-const http = require('http');
-const PORT = process.env.arg || 4000;
 const ejs = require('ejs')
+const socket = require('socket.io')
+const http = require('http');
+
+const server = http.createServer(app);
+const app = express();
+const io = socket(server)
+const PORT = process.env.arg || 4000;
+
 
 app.engine('html', ejs.renderFile)
 app.set('view engine', 'html')
 
-const server = http.createServer(app);
 
 app.get('/', (req, res) => {
-
+    res.send('OK')
 });
 
 server.listen(PORT, () => console.log(PORT));
